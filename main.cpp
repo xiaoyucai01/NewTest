f30a1c3be167d48de122883386724a5d87a291b9
xiao yucai <1065838180@qq.com>
2022-12-09 19:49:43 +0800

    change main.cpp

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main()
   std::cout << "hellow world!" << std::endl;
   std::cout << "hellow world!02." << std::endl;
   std::cout << "hellow world!03." << std::endl;
+  std::cout << "hellow world!04." << std::endl;
   return 0;
 }
